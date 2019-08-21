977a822a6890889ec9bfd3600aa83f9f07473678
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2019-08-22 01:32:55 +0530

    Added javascript file for the web page interactions

---
+++ Interpret a Data Visualization/designs.js
@@ -0,0 +1,20 @@
+document.getElementById("nanodegree-details").onclick = function () {
+    var url = "https://www.udacity.com/course/business-analytics-nanodegree--nd098";
+    window.open(url, "_blank");
+    // alternatively use the below code to open in the link same tab (undesired action)
+    // location.href = "https://www.udacity.com/course/business-analytics-nanodegree--nd098";
+};
+
+document.getElementById("project-ruberic").onclick = function () {
+    var url = "https://review.udacity.com/#!/rubrics/1063/view";
+    window.open(url, "_blank");
+    // alternatively use the below code to open in the link same tab (undesired action)
+    // location.href = "https://review.udacity.com/#!/rubrics/1063/view";
+};
+
+document.getElementById("github").onclick = function () {
+    var url = "https://github.com/vamshi-krishna-prime/Business_Analytics";
+    window.open(url, "_blank");
+    // alternatively use the below code to open in the link same tab (undesired action)
+    // location.href = "https://github.com/vamshi-krishna-prime/Business_Analytics";
+};
