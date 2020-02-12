89478806b4d1134c26a6bc2964d3919b821cf26f
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-12 23:40:15 +0530

    SQL > Basics > Added Query 29

---
+++ 2. Introduction to SQL/Lesson 1 - SQL Basics/29-not-like-companies-start-with-C.sql
@@ -0,0 +1,12 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Use the accounts table to find:
+All the companies whose names do not start with 'C'.
+*/
+
+
+SELECT name
+FROM accounts
+WHERE name NOT LIKE 'C%';
