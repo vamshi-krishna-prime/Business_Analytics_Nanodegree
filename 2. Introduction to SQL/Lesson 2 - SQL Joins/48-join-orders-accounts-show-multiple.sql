6c01374e5361e165681a1522418ecb1ff3304b70
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-12 23:49:19 +0530

    SQL > Basics > Added Query 48

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/48-join-orders-accounts-show-multiple.sql
@@ -0,0 +1,13 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Try pulling 'standard_qty', 'gloss_qty', and 'poster_qty' from the 'orders'
+table, and the 'website' and the 'primary_poc' from the 'accounts' table.
+*/
+
+
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.website, accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
