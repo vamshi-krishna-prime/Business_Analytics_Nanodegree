fd05e83a6d056e60cd23ee4089f9b706991fed05
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-12 23:49:37 +0530

    SQL > Basics > Added Query 49

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/49-join-multiple-tables.sql
@@ -0,0 +1,15 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Pull data related to 'channel' from 'web_events' table, 'name' from 'accounts'
+table and 'total' from 'orders' table
+*/
+
+
+SELECT web_events.channel, accounts.name, orders.total
+FROM web_events
+JOIN accounts
+ON web_events.account_id = accounts.id
+JOIN orders
+ON accounts.id = orders.account_id; 
