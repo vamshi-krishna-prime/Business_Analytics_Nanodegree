fa3ba791e637611a8c5d4f8b0306328c0a1046cd
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:46:25 +0530

    SQL > Basics > Added Query 64

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/64-join-between-and-q8-orders-occurred_at-2105.sql
@@ -0,0 +1,16 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Find all the orders that occurred in 2015. Your final table should have
+4 columns: occurred_at, account name, order total, and order total_amt_usd.
+*/
+
+
+
+SELECT o.occurred_at occurred_at, a.name account, o.total total_order, o.total_amt_usd total_price
+FROM order o
+JOIN accounts a
+ON a.id = o.account_id
+WHERE o.occurred_at BETWEEN '1-1-2015' AND '1-1-2016'
+ORDER BY o.occurred_at DESC;
