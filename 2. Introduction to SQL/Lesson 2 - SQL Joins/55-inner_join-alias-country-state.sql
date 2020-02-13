31ac145f4a0b8567bdd990c3c8cb50f394d854f2
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:43:51 +0530

    SQL > Basics > Added Query 55

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/55-inner_join-alias-country-state.sql
@@ -0,0 +1,15 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Make an INNER JOIN to the 'Country' and 'State' table provided in the
+SQL section, lesson 2, section 17.
+Display 'countryid', 'countryName' and 'stateName'
+*/
+
+
+
+SEELECT c.countryid, c.countryName, s.stateName
+FROM Country c
+JOIN State s
+ON c.countryid = s.countryid;
