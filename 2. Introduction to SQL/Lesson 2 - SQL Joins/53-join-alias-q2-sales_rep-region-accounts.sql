610b4b471511b912b007fcbf5d1dd12d6c6d1f56
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:43:21 +0530

    SQL > Basics > Added Query 53

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/53-join-alias-q2-sales_rep-region-accounts.sql
@@ -0,0 +1,32 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Provide a table that provides the region for each sales_rep along with their
+associated accounts. Your final table should include three columns:
+the region name, the sales rep name, and the account name.
+Sort the accounts alphabetically (A-Z) according to account name.
+*/
+
+
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+ORDER BY a.name;
+
+
+/*
+If you have two or more columns in your SELECT that have the same name after
+the table name such as accounts.name and sales_reps.name you will need to
+alias them. Otherwise it will only show one of the columns. You can alias
+them like accounts.name AS AcountName, sales_rep.name AS SalesRepName
+
+For example in the above query, if we write
+`SELECT r.name region, s.name rep, a.name account`
+then the result will have only one column from `a.name account` because
+all three columns have same name, it is overwritten twice.
+*/
