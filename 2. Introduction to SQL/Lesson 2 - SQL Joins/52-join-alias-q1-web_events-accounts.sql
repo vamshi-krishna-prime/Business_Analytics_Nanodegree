b9f66eb10cd99154e3ef375355ff48d268680e14
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:43:06 +0530

    SQL > Basics > Added Query 52

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/52-join-alias-q1-web_events-accounts.sql
@@ -0,0 +1,17 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Provide a table for all 'web_events' associated with account 'name' of
+'Walmart'. There should be three columns. Be sure to include the 'primary_poc',
+time of the event, and the 'channel' for each event. Additionally, you might
+choose to add a fourth column to assure only 'Walmart' events were chosen.
+*/
+
+
+
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
