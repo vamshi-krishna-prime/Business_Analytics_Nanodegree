44c830c65b70052b3fb4dae1a94b77e5b92621f9
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:43:36 +0530

    SQL > Basics > Added Query 54

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/54-join-alias-q3-complex-region-accounts-orders.sql
@@ -0,0 +1,21 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Provide the name for each region for every order, as well as the account name
+and the unit price they paid (total_amt_usd/total) for the order. Your final
+table should have 3 columns: region name, account name, and unit price. A few
+accounts have 0 for total, so I divided by (total + 0.01) to assure not
+dividing by zero.
+*/
+
+
+
+SELECT r.name region, a.name account, (o.total_amt_usd/(o.total + 0.01)) unit_price
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id;
