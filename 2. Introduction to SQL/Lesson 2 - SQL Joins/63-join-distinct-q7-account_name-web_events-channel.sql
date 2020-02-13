9f9ffd09628b7722b3a45b8933859daa244087dd
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:46:09 +0530

    SQL > Basics > Added Query 63

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/63-join-distinct-q7-account_name-web_events-channel.sql
@@ -0,0 +1,16 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+What are the different channels used by account id 1001? Your final table
+should have only 2 columns: account name and the different channels. You can
+try SELECT DISTINCT to narrow down the results to only the unique values.
+*/
+
+
+
+SELECT DISTINCT a.name account, w.channel channel
+FROM accounts a
+JOIN web_events w
+ON w.account_id = a.id
+WHERE a.id = 1001;
