77f8a0c82c2adb7fc8165e85ef971ee595912061
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:44:50 +0530

    SQL > Basics > Added Query 59

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/59-join-alias-q3-region-sales_rep-name-with-K-lastname.sql
@@ -0,0 +1,21 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Provide a table that provides the region for each sales_rep along with their
+associated accounts. This time only for accounts where the sales rep has a
+last name starting with K and in the Midwest region. Your final table should
+include three columns: the region name, the sales rep name, and the
+account name. Sort the accounts alphabetically (A-Z) according to account name.
+*/
+
+
+
+SELECT r.name region, s.name sales_rep, a.name account
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+WHERE s.name LIKE '% K%' AND r.name = 'Midwest'
+ORDER BY a.name;
