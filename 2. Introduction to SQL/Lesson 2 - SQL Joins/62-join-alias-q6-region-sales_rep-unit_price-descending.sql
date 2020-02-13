21db6a82b34deeb04fdc4d650a483906475a6c44
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-13 22:45:52 +0530

    SQL > Basics > Added Query 62

---
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/62-join-alias-q6-region-sales_rep-unit_price-descending.sql
@@ -0,0 +1,25 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 1 : SQL Basics
+
+Provide the name for each region for every order, as well as the account name
+and the unit price they paid (total_amt_usd/total) for the order. However, you
+should only provide the results if the standard order quantity exceeds 100 and
+the poster order quantity exceeds 50. Your final table should have 3 columns:
+region name, account name, and unit price. Sort for the largest unit price
+first. In order to avoid a division by zero error, adding .01 to the
+denominator here is helpful (total_amt_usd/(total+0.01).
+*/
+
+
+
+SELECT r.name region, a.name account, o.total_amt_usd/(o.total + 0.01) unit_price
+FROM region r
+JOIN sales_reps s
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price DESC;
