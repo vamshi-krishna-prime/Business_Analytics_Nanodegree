c49eadd595cd653876042711f364994251d4c9d6
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:40:31 +0530

    SQL > Joins > Added Query 69

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/69-sum-q2-orders-total-standard-sales.sql
@@ -0,0 +1,11 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the total amount of 'standard_qty' paper ordered in the 'orders' table.
+*/
+
+
+
+SELECT SUM(o.standard_qty) total_standard_sales
+FROM orders o;
