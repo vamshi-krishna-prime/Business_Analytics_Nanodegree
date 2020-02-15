29e67e72324c3a5d9592532b8c26110e186a4ead
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:41:26 +0530

    SQL > Joins > Added Query 72

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/72-sum_and_aggregation-q5-standard_price_per_unit.sql
@@ -0,0 +1,12 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the 'standard_amt_usd' per unit of 'standard_qty' paper. Your solution
+should use both an aggregation and a mathematical operator.
+*/
+
+
+
+SELECT SUM(o.standard_amt_usd) / SUM(o.standard_qty) AS standard_price_per_unit
+FROM orders o;
