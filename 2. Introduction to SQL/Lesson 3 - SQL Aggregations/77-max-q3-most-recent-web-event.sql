e3112081ffdcdbb976cce756ac0c3560e0b4dc7b
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:43:09 +0530

    SQL > Joins > Added Query 77

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/77-max-q3-most-recent-web-event.sql
@@ -0,0 +1,11 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+When did the most recent (latest) web_event occur?
+*/
+
+
+
+SELECT MAX(w.occurred_at) most_recent_web_event
+FROM web_events w;
