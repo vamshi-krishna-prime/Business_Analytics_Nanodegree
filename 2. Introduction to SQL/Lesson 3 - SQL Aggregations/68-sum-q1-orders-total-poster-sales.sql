e7ae467d62c9a00828f88c65b4f30ebe31b95fa1
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:40:17 +0530

    SQL > Joins > Added Query 68

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/68-sum-q1-orders-total-poster-sales.sql
@@ -0,0 +1,11 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the total amount of 'poster_qty' paper ordered in the 'orders' table.
+*/
+
+
+
+SELECT SUM(o.poster_qty) total_poster_sales
+FROM orders o;
