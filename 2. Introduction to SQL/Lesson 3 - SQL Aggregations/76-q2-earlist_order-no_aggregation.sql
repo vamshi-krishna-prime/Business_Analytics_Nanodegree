ce6eb6202477493ad34d76d10610c28efa0f55a6
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:42:54 +0530

    SQL > Joins > Added Query 76

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/76-q2-earlist_order-no_aggregation.sql
@@ -0,0 +1,17 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+When was the earliest order (oldest) ever placed? You only need to return
+the date.
+
+Try performing the same query as in question 1 without using an
+aggregation function.
+*/
+
+
+
+SELECT o.occurred_at earliest_order
+FROM orders o
+ORDER BY o.occurred_at
+LIMIT 1;
