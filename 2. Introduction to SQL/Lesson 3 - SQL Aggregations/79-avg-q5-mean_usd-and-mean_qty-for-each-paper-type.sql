96c7d3992834b81828b7ea415dc6851e55a56e04
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:43:38 +0530

    SQL > Joins > Added Query 79

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/79-avg-q5-mean_usd-and-mean_qty-for-each-paper-type.sql
@@ -0,0 +1,14 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the mean (AVERAGE) amount spent per order on each paper type, as well as
+the mean amount of each paper type purchased per order. Your final answer
+should have 6 values - one for each paper type for the average number of sales,
+as well as the average amount.
+*/
+
+
+
+SELECT AVG(o.standard_amt_usd) standard_avg_usd, AVG(o.gloss_amt_usd) gloss_avg_usd, AVG(o.poster_amt_usd) poster_avg_usd, AVG(o.standard_qty) standard_avg_qty, AVG(o.gloss_qty) gloss_avg_qty, AVG(o.poster_qty) gloss_avg_qty
+FROM orders o;
