923567c0e97a797ca5a6e8d4b0533087f9d1ea7e
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:40:04 +0530

    SQL > Joins > Added Query 67

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/67-count-orders-poster_qty.sql
@@ -0,0 +1,11 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the total no of 'poster_qty' paper orders in the 'orders' table.
+*/
+
+
+
+SELECT COUNT(o.poster_qty) total_poster_qty
+FROM orders o;
