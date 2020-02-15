e1ce79189a3adc239523d3763acaf46ee7969b22
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:43:23 +0530

    SQL > Joins > Added Query 78

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/78-q4-most-recent-web-event-no_aggregation.sql
@@ -0,0 +1,16 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+When did the most recent (latest) web_event occur?
+
+Try to perform the result of the previous query without using an
+aggregation function.
+*/
+
+
+
+SELECT w.occurred_at most_recent_web_event
+FROM web_events w
+ORDER BY w.occurred_at DESC
+LIMIT 1;
