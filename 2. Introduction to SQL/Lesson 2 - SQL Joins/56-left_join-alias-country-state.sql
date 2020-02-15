f8831841137ed37b0b0a90606fbc42001d1d43ff
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:25:21 +0530

    SQL > Joins > Added Query 56

--- 2. Introduction to SQL/Lesson 2 - SQL Joins/56-left_join-alias-country-state.sql
+++ 2. Introduction to SQL/Lesson 2 - SQL Joins/56-left_join-alias-country-state.sql
@@ -1,6 +1,6 @@
 /*
 Udacity's certified program > Business Analytics Nanodegree
-SQL practice > Lesson 1 : SQL Basics
+SQL practice > Lesson 2 : SQL Joins
 
 Make a LEFT JOIN to the 'Country' and 'State' table provided in the
 SQL section, lesson 2, section 17.
