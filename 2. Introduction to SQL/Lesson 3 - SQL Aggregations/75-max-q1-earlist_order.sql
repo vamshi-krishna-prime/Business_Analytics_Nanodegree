65ed65df0b3f8e930c3bbd782ea0b17e13669165
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:42:15 +0530

    SQL > Joins > Added Query 75

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/75-max-q1-earlist_order.sql
@@ -0,0 +1,12 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+When was the earliest order (oldest) ever placed? You only need to return
+the date.
+*/
+
+
+
+SELECT MIN(o.occurred_at) earliest_order
+FROM orders o;
