11327b1d3ca49a7ae40bf7c8406bac3893bd96b6
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:42:00 +0530

    SQL > Joins > Added Query 74

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/74-average-order_size-for-each-type.sql
@@ -0,0 +1,30 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the average order value received by the 'parch and posey' company
+regarding standard paper, gloss paper and poster paper
+*/
+
+
+
+SELECT AVG(o.standard_qty) standard_qty_average_order, AVG(o.gloss_qty) gloss_qty_average_order, AVG(o.poster_qty) poster_qty_average_order
+FROM order o;
+
+
+/*
+Similar to other software AVG returns the mean of the data - that is the sum
+of all of the values in the column divided by the number of values in a column.
+This aggregate function again ignores the NULL values in both the numerator and
+the denominator.
+
+If you want to count NULLs as zero, you will need to use SUM and COUNT.
+However, this is probably not a good idea if the NULL values truly just
+represent unknown values for a cell.
+
+MEDIAN - Expert Tip
+One quick note that a median might be a more appropriate measure of center for
+this data, but finding the median happens to be a pretty difficult thing to get
+using SQL alone — so difficult that finding a median is occasionally asked as
+an interview question.
+*/
