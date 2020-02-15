7d05b5b912753470d8234adfadbd46f43e3d9f1a
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:39:07 +0530

    SQL > Joins > Added Query 65

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/65-count-aggregation.sql
@@ -0,0 +1,11 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+find the count of id's in the accounts table.
+*/
+
+
+
+SELECT COUNT(*)
+FROM accounts;
