1d8ab9ff68e94cb04a81f051baf9859a89101a88
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-15 23:40:46 +0530

    SQL > Joins > Added Query 70

---
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/70-sum-q3-orders-total-dollar-amount.sql
@@ -0,0 +1,12 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 3 : SQL Aggregations
+
+Find the total dollar amount of sales using the 'total_amt_usd' in
+the 'orders' table.
+*/
+
+
+
+SELECT SUM(o.total_amt_usd) total_dollar_amount
+FROM orders o;
