eda05650fb7344e98d71700bec6f982b9f840e91
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-16 23:40:33 +0530

    SQL > Aggregations > Modified Query 79

--- 2. Introduction to SQL/Lesson 3 - SQL Aggregations/79-avg-q5-mean_usd-and-mean_qty-for-each-paper-type.sql
+++ 2. Introduction to SQL/Lesson 3 - SQL Aggregations/79-avg-q5-mean_usd-and-mean_qty-for-each-paper-type.sql
@@ -9,6 +9,5 @@ as well as the average amount.
 */
 
 
-
 SELECT AVG(o.standard_amt_usd) standard_avg_usd, AVG(o.gloss_amt_usd) gloss_avg_usd, AVG(o.poster_amt_usd) poster_avg_usd, AVG(o.standard_qty) standard_avg_qty, AVG(o.gloss_qty) gloss_avg_qty, AVG(o.poster_qty) gloss_avg_qty
 FROM orders o;
