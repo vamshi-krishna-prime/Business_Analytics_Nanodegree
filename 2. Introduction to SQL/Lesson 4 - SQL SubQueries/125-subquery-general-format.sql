00210fa67929f22387ea0787beec611a5033ad36
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-18 23:12:34 +0530

    SQL > SubQueries > Added Query 125

---
+++ 2. Introduction to SQL/Lesson 4 - SQL SubQueries/125-subquery-general-format.sql
@@ -0,0 +1,22 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 4 : SQL SubQueries
+
+SUB QUERY general format
+*/
+
+SELECT *
+FROM (
+     ) Alias_name
+GROUP BY
+ORDER BY ;
+
+
+/*
+Format Rules:
+
+1. The original query goes in the FROM statement.
+2. An * is used in the SELECT statement to pull all of the data from the
+original query (if needed to pull all the data inside the sub-query).
+3. You MUST use an alias for the table you nest within the outer query.
+*/
