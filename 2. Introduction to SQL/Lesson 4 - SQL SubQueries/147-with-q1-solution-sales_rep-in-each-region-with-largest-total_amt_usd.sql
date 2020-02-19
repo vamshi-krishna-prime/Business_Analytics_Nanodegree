0956bf4ac98a2ed56b7176eb6b9eea6818f23ba8
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-19 23:06:35 +0530

    SQL > SubQueries > Added Query 147

---
+++ 2. Introduction to SQL/Lesson 4 - SQL SubQueries/147-with-q1-solution-sales_rep-in-each-region-with-largest-total_amt_usd.sql
@@ -0,0 +1,33 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 4 : SQL SubQueries
+
+WITH Statements:
+
+Question 1:
+Provide the name of the sales_rep in each region with the largest amount of
+total_amt_usd sales.
+*/
+
+/* Same question as 130,131 ; but Query using WITH statement */
+
+
+WITH t1 AS (
+  SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+   FROM sales_reps s
+   JOIN accounts a
+   ON a.sales_rep_id = s.id
+   JOIN orders o
+   ON o.account_id = a.id
+   JOIN region r
+   ON r.id = s.region_id
+   GROUP BY 1,2
+   ORDER BY 3 DESC),
+t2 AS (
+   SELECT region_name, MAX(total_amt) total_amt
+   FROM t1
+   GROUP BY 1)
+SELECT t1.rep_name, t1.region_name, t1.total_amt
+FROM t1
+JOIN t2
+ON t1.region_name = t2.region_name AND t1.total_amt = t2.total_amt;
