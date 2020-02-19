35fe1bbd83e2997035ff4b7e85ffa734499ddc2b
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-19 23:07:06 +0530

    SQL > SubQueries > Added Query 149

---
+++ 2. Introduction to SQL/Lesson 4 - SQL SubQueries/149-with-q2-solution-total-num_orders-for-region-with-largest-total_amt_usd.sql
@@ -0,0 +1,39 @@
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 4 : SQL SubQueries
+
+WITH Statements:
+
+Question 2:
+For the region with the largest sales total_amt_usd, how many total orders
+were placed?
+*/
+
+/* Same question as 132,133 ; but Query using WITH statement */
+
+/*STEP1: */
+
+
+WITH t1 AS (
+   SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
+   FROM sales_reps s
+   JOIN accounts a
+   ON a.sales_rep_id = s.id
+   JOIN orders o
+   ON o.account_id = a.id
+   JOIN region r
+   ON r.id = s.region_id
+   GROUP BY r.name),
+t2 AS (
+   SELECT MAX(total_amt)
+   FROM t1)
+SELECT r.name, COUNT(o.total) total_orders
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (SELECT * FROM t2);
