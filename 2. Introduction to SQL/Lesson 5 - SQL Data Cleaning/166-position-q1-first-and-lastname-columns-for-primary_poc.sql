0f87c7cb6e72cbcd381ebac2814f1e461b0678bc
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-21 00:49:04 +0530

    SQL > Data Cleaning > Added Query 166

---
+++ 2. Introduction to SQL/Lesson 5 - SQL Data Cleaning/166-position-q1-first-and-lastname-columns-for-primary_poc.sql
@@ -0,0 +1,30 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+SQL > Lesson 5 > SQL Data cleaning
+
+Quizzes POSITION & STRPOS:
+You will need to use what you have learned about LEFT & RIGHT, as well as what
+you know about POSITION or STRPOS to do the following quizzes.
+
+Question 1:
+Use the accounts table to create first and last name columns that hold the
+first and last names for the primary_poc.
+*/
+
+SELECT LEFT(a.primary_poc, POSITION(' ' IN a.primary_poc) - 1) AS first_name,
+       RIGHT(a.primaty_poc, (LENGTH(a.primary_poc) - STRPOS(a.primary_poc, ' '))) AS lastname
+FROM accounts a;
+
+/*
+Output 351 results:
+
+first_name	     lastname
+Tamara	         Tuma
+Sung	           Shields
+Jodee	           Lupo
+Serafina	       Banda
+Angeles	         Crusoe
+.
+.
+and 346 more results
+*/
