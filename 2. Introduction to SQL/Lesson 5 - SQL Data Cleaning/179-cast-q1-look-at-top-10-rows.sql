28ba52fdfe28ee11c60b79820ac097d661be342e
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-21 01:00:14 +0530

    SQL > Data Cleaning > Added Query 179

---
+++ 2. Introduction to SQL/Lesson 5 - SQL Data Cleaning/179-cast-q1-look-at-top-10-rows.sql
@@ -0,0 +1,20 @@
+/*
+SQL practice in Udacity's Business Analytics Nanodegree Program
+SQL > Lesson 5 > SQL Data cleaning
+
+CAST Quizzes
+
+For this set of quiz questions, you are going to be working with a single table
+in the environment below. This is a different dataset than Parch & Posey, as
+all of the data in that particular dataset were already clean.
+
+-----------Note: Database for these queries are not available locally-----------
+
+Question 1:
+Write a query to look at the top 10 rows to understand the columns and the raw
+data in the dataset called 'sf_crime_data'
+*/
+
+SELECT *
+FROM sf_crime_data
+LIMIT 10;
