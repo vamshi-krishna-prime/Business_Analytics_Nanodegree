8f934700c400014c752a99dc364cd18cbffbb04f
VAMSHI-KRISHNA-PRIME <vamshi.krishna.prime@gmail.com>
2020-02-24 23:41:46 +0530

    SQL > Advanced Joins > Modified Query 225

--- 2. Introduction to SQL/Lesson 7 - Advanced Joins/225-advanced_joins-inner-left-right-and-full_outer-joins.sql
+++ 2. Introduction to SQL/Lesson 7 - Advanced Joins/225-advanced_joins-inner-left-right-and-full_outer-joins.sql
@@ -1,6 +1,6 @@
-/*  DOUBT : why final solution not working ?
-SQL practice in Udacity's Business Analytics Nanodegree Program
-SQL > Lesson 7 > SQL Advanced JOINs and Performance Tuning
+/*
+Udacity's certified program > Business Analytics Nanodegree
+SQL practice > Lesson 7 : SQL Advanced JOINs
 
 Advanced JOINs:
 
